96fec0d00ca9b4260d25f1390a2f90509ba93d68
joseluisgomezcecena <joseluisgomezcecegna@gmail.com>
2023-02-28 17:30:45 -0800

    First commit.

---
+++ application/controllers/Pages.php
@@ -0,0 +1,22 @@
+<?php
+
+class Pages extends CI_Controller
+{
+	public function view($page = 'home')
+	{
+		if(!file_exists(APPPATH . 'views/pages/' . $page . '.php'))
+		{
+			show_404();
+		}
+
+		$data['title'] = ucfirst($page);
+
+		//load header, page & footer
+		//$this->load->view('templates/header');
+		//$this->load->view('templates/sidebar');
+		//$this->load->view('templates/workspace_start');
+		$this->load->view('pages/' . $page, $data); //loading page and data
+		//$this->load->view('templates/footer');
+	}
+
+}
---
+++ application/views/pages/home.php
@@ -0,0 +1,3 @@
+<h1> <?= $title ?> </h1>
+
+<p>Hi! we can also output like this: <?php echo $title ?></p>
